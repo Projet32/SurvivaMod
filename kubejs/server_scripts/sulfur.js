a48e461cc1c195ee7301f4395002289dad8d97fc
SuperCameleon <126679563+SuperCameleon@users.noreply.github.com>
2023-11-03 18:22:36 +0100

    Patch temporaire pour le sulfur
    Patch temporaire fait en 30s

---
+++ kubejs/server_scripts/sulfur.js
@@ -0,0 +1,4 @@
+ServerEvents.tags('item', event => {
+    // Get the #forge:cobblestone tag collection and add Diamond Ore to it
+    event.add('forge:dusts/sulfur', 'sulfurpotassiummod:sulfur')
+})
